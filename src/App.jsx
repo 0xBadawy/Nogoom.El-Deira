924a9e2f1dec68022e9ef2a194833ece32172d6b
Mohamed Badawy <0xBadawy@gmail.com>
2024-11-22 23:17:28 +0200

    Add type definitions and exports for various resolvers and libraries

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./Components/NavBar/Navbar";
 import HomePage from "./HomePage/HomePage";
+import LoginPage from "./Stars/LoginPage";
 
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ]);
 
